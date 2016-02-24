a21b44c56c2ca90ac4cef5792538286aae99580b
Jenny lian <jennylian100@gmail.com>
2016-02-23 20:24:04 -0800

    added a map component

--- app/scripts/components/home.jsx
+++ app/scripts/components/home.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var Button = require('react-bootstrap').Button;
 var Well = require('react-bootstrap').Well;
 var ImageGrid = require('./imagegrid');
+var Map = require('./map');
 var ImageActions = require('../actions/imageActions');
 
 var Home = React.createClass({
@@ -14,9 +15,12 @@ var Home = React.createClass({
           <h1>Travel Journal</h1>
           <Button bsStyle='primary' onClick={ImageActions.fetchList}>More Photos</Button>
           <Well>
+            <Map></Map>
           </Well>
         </Well>
+
         <Well>
+
           <ImageGrid></ImageGrid>
         </Well>
       </div>
---
+++ app/scripts/components/map.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Map = React.createClass({
+
+  render: function() {
+
+    return (
+      <div>
+        <p>A Map</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Map;
