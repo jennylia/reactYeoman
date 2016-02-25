9a1c22343bfcc30eda07fd1e4925b19a760d7d3d
Jenny lian <jennylian100@gmail.com>
2016-02-24 18:03:29 -0800

    adding the location item and list

---
+++ app/scripts/components/locationitem.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Locationitem = React.createClass({
+
+  render: function() {
+
+    return (
+      <div>
+        <p>Your content</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Locationitem;
---
+++ app/scripts/components/locationlist.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Locationlist = React.createClass({
+
+  render: function() {
+
+    return (
+      <div>
+        <p>Your content</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Locationlist;
